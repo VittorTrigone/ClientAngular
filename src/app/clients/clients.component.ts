5ed4a02501350e4d85123898dfd64c9759998a6b
VittorTrigoneDias <Vittor.Trigone.Dias@gmail.com>
2023-05-02 16:50:53 -0300

    ClientAngular

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { ClientService } from './../client.service';
+import { Component, OnInit } from '@angular/core';
+import { Client } from '../client';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent {
+export class ClientsComponent implements OnInit {
 
+  clients : Client[] = [];
+
+  constructor (private ClientService : ClientService) {}
+
+  ngOnInit(): void {
+    this.loadClients ()
+  }
+
+  loadClients() {
+    this.ClientService.getClients().subscribe (
+      {
+        next : data => this.clients = data
+      }
+    );
+  }
 }
